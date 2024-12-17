2cec179f099155cde0c31359478bb2dc61a407dd
chirag1294 <anghan.chirag1212@gmail.com>
2024-12-17 15:19:00 -0500

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Chirag Anghan on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
